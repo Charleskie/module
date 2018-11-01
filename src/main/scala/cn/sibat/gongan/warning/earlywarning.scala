47b47ebbcfcb4216eea494e438e11130a5f182ec
Charleskie <13531158704@163.com>
2018-11-01 18:45:39 +0800

    部分计算

--- src/main/scala/cn/sibat/gongan/warning/earlywarning.scala
+++ src/main/scala/cn/sibat/gongan/warning/earlywarning.scala
@@ -1,8 +1,6 @@
 package cn.sibat.gongan.warning
 
 import java.text.{DateFormat, SimpleDateFormat}
-import java.util.Date
-
 import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.hadoop.mapred.TextInputFormat
 import org.apache.spark.rdd.RDD
@@ -13,13 +11,17 @@ object earlywarning{
   def main(args: Array[String]): Unit = {
     val sparkSession = SparkSession.builder().master("local[*]").getOrCreate()
     val sc = sparkSession.sparkContext
-    val data =   sparkSession.sparkContext.hadoopFile[LongWritable,Text,TextInputFormat](path+"early_warning1031.txt")
+    val data =   sparkSession.sparkContext.hadoopFile[LongWritable,Text,TextInputFormat](path+"early_warning1101.txt")
       .map(p=> new String(p._2.getBytes,0,p._2.getLength,"GBK"))
 
 //    val date = sparkSession.read.textFile(path+"early_warning1029.xls").rdd.foreach(println)
 
 //    data.foreach(println)
-    calDayCount(data)
+//    calDayCount(data)
+//    calHourCount(data)
+//    calStationCount(data)
+//    calTypeCount(data)
+    calSimilarityCount(data)
 
   }
 
@@ -31,14 +33,14 @@ object earlywarning{
     rdd.map(s => {
       val line = s.split(",")
       (line(0),string2time(line(12)).substring(0,10))
-    }).groupBy(s => s._2).map(s => s._1+","+s._2.size).coalesce(1).saveAsTextFile(path+"dayCount")
+    }).groupBy(s => s._2).map(s => s._1+","+s._2.size).coalesce(1).saveAsTextFile(path+"out/dayCount")
   }
 
   def calHourCount(rdd: RDD[String]):Unit ={
     rdd.map(s=> {
       val line = s.split(",")
       (line(0),string2time(line(12)).substring(10,13))
-    }).groupBy(s=> s._2).map(s=> s._1+","+s._2.size).coalesce(1).saveAsTextFile(path+"hourCount")
+    }).groupBy(s=> s._2).map(s=> s._1+","+s._2.size).coalesce(1).saveAsTextFile(path+"out/hourCount")
   }
 
   /***
@@ -62,7 +64,7 @@ object earlywarning{
     rdd.map(s =>{
       val ss = s.split(",")
       (ss(0),ss(9))
-    }).groupBy(s=>s._2).map(s=> s._1+","+s._2.size).saveAsTextFile(path+"stationCount")
+    }).groupBy(s=>s._2).map(s=> s._1+","+s._2.size).coalesce(1).saveAsTextFile(path+"out/stationCount")
   }
 
   def calTypeCount(rdd: RDD[String]): Unit ={
@@ -72,8 +74,8 @@ object earlywarning{
     }).groupBy(s => (s._3,s._2)).map(s => s._1+","+s._2.size).map(s=>{
       val line = s.replaceAll("\\(","").replaceAll("\\)","").split(",")
       val sum = line.size
-      s(1)+","+s(2)+","+s(3)+","+s(3).toDouble/sum
-    }).foreach(println)
+      line(0)+","+line(1)+","+line(2)+","+line(2).toDouble/sum
+    }).coalesce(1).saveAsTextFile(path+"out/typecount")
   }
 
   def calSimilarityCount(rdd: RDD[String]):Unit={
@@ -81,5 +83,10 @@ object earlywarning{
       .groupBy(s => s._2.substring(0,5)).map(s => s._1.replaceAll("","").replaceAll("\\)","")+","+s._2.size)
       .foreach(println)
   }
+
+  case class earlywarning(id: String, device_id: String, device_type:String, device_address:String, data_sources:String ,
+                          keyperson_id:String ,keyperson_state:String,keyperson_type:String ,event_address_id:String ,
+                          event_address_name:String ,event_status:String ,compare_sources:String ,create_time:String ,
+                          update_time:String ,convictions:String ,job_name:String ,name:String ,pid:String ,taskid:String ,similarity:String )
 
 }

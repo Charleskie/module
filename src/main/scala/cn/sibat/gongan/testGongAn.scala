c6e8fac5aed0a601030251f7919e3a8183c593dd
Charleskie <13531158704@163.com>
2018-12-03 19:26:01 +0800

    读取深圳通的刷卡字段信息

--- src/main/scala/cn/sibat/gongan/testGongAn.scala
+++ src/main/scala/cn/sibat/gongan/testGongAn.scala
@@ -1,11 +1,19 @@
 package cn.sibat.gongan
-import cn.sibat.gongan.warning.earlywarning.string2time
+
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.hadoop.mapred.TextInputFormat
+import org.apache.spark.sql.SparkSession
 
 object testGongAn{
   def main(args: Array[String]): Unit = {
-    val date = string2time("8/8/2018 10:00:00")
-    println(date)
-//    println(date.getDay,date.getDate,date.getHours,date.getMonth+1,date.getYear)
+    val sparkSession = SparkSession.builder().master("local[*]").getOrCreate()
+    val sc = sparkSession.sparkContext
+    val path = "C:\\Users\\小怪兽\\Desktop\\Kim1023\\data\\"
+    val data = sparkSession.sparkContext.hadoopFile[LongWritable, Text, TextInputFormat](path + "szt20181114\\200_2018111400.csv")
+      .map(p => new String(p._2.getBytes, 0, p._2.getLength, "GBK"))
+
+    data.take(1).foreach(println)
+    data.filter(s => s.contains("M476")).take(100).foreach(println)
 
   }
 }

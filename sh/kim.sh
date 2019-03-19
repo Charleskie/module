4f0289eae7d459750d596ed0b5bfb074521ee670
Charleskie <13531158704@163.com>
2019-03-19 20:46:16 +0800

    更新启动脚本文件

--- sh/kim.sh
+++ sh/kim.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 source ~/.bashrc
-rm -rf metastore_db/;
-sh /data/home/hadoop/wj/startQuery.sh < /data/home/hadoop/kim/code/kim.scala;
-DATE=$(date -d yesterday +"%Y-%m-%d");
-hadoop fs -cat Kim/data/out/${DATE}/pa* > /data/home/hadoop/kim/data/${DATE}.txt;
-rm -rf metastore_db;
+rm -r /data/home/hadoop/kim/code/metastore_db/
+rm -r metastore_db
+sh /data/home/hadoop/wj/startQuery.sh < /data/home/hadoop/kim/code/kim.scala
+DATE_HADOOP=$(date -d yesterday +"%Y-%m-%d")
+DATE_LOCAL=$(date -d yesterday +"%Y%m%d")
+MONTH=$(date -d yesterday +"%Y%m")
+hadoop fs -cat Kim/data/out/${DATE_HADOOP}/pa* > /data/home/hadoop/DoubleKing/${MONTH}/${DATE_LOCAL}/warning/warning${DATE_LOCAL}.txt
+rm -r metastore_db
